8d2486dda00ad75a7665ef4537978a6448120184
CmarieS <marie-sophie.cuer@ynov.com>
2022-12-15 17:29:16 +0100

    add navigation, banner and css

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import logo from '../pictures/banner.png'; 
+
+function Banner() {
+    return (
+        <div className='banner'>
+            <img src={logo} alt="Logo" />
+        </div>
+    )
+};
+
+export default Banner;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,11 @@
+import Navigation from "../components/Navigation";
+import Banner from "../components/Banner";
+
 function Home() {
     return (
         <div class="app">
+            <Navigation />
+            <Banner />
             <h1>Home</h1>
         </div>
     )
